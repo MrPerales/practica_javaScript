a7208c74ad567c535f52780a3cf3ef775632a284
Carlos Perales <carlosperales050@gmail.com>
2022-12-20 16:39:54 -0600

    Calculadora simple con input radio

--- script.js
+++ script.js
@@ -4,7 +4,34 @@ const input2=document.querySelector("#calculo2");
 const btn= document.querySelector("btnCalcular");
 const pResultado=document.querySelector("#resultado");
 
+const opcSuma=document.querySelector("#suma");
+const opcResta=document.querySelector("#resta");
+const opcDiv=document.querySelector("#division");
+const opcMulti=document.querySelector("#multiplicacion")
+
 function btnOnClick(){
-    const sumaImputs= Number(input1.value) + Number(input2.value);
-    pResultado.innerText= "resultado: " + sumaImputs;
+
+    if (opcSuma.checked){     ////.checked para verificar si esta precionado o no 
+        const sumaImputs= Number(input1.value) + Number(input2.value);
+        pResultado.innerText= "resultado: " + sumaImputs;
+        //console.log("suma");
+    
+    }else if (opcResta.checked){
+        const sumaImputs= Number(input1.value) - Number(input2.value);
+        
+        pResultado.innerText= "resultado: " + sumaImputs;
+        //console.log("r");
+    }else if (opcDiv.checked){
+        const sumaImputs= Number(input1.value) / Number(input2.value);
+        pResultado.innerText= "resultado: " + sumaImputs;
+        //console.log("d");
+
+    }else if (opcMulti.checked){
+        const sumaImputs= Number(input1.value) * Number(input2.value);
+        pResultado.innerText= "resultado: " + sumaImputs;
+        //console.log("m");
+    }
+    
+   //const sumaImputs= Number(input1.value) + Number(input2.value);
+ // pResultado.innerText= "resultado: " + sumaImputs;
 }
